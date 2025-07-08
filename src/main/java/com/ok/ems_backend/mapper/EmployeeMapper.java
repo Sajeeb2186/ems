8dc2930713bc2fad90853d18327767366d993e6d
Sajeeb <sajeeb2186@gmail.com>
2025-07-08 23:45:38 +0600

    added more code for backend

--- src/main/java/com/ok/ems_backend/mapper/EmployeeMapper.java
+++ src/main/java/com/ok/ems_backend/mapper/EmployeeMapper.java
@@ -1,4 +1,33 @@
 package com.ok.ems_backend.mapper;
 
+import com.ok.ems_backend.dto.EmployeeDto;
+import com.ok.ems_backend.entity.Employee;
+
 public class EmployeeMapper {
+
+    public static EmployeeDto mapToEmployeeDto(Employee employee){
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail()
+
+
+        );
+
+
+    }
+
+
+    public static Employee mapToEmployee(EmployeeDto employeeDto){
+        return new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                employeeDto.getEmail()
+
+        );
+
+    }
+
 }
---
+++ src/main/java/com/ok/ems_backend/service/EmployeeService.java
@@ -0,0 +1,7 @@
+package com.ok.ems_backend.service;
+
+import com.ok.ems_backend.dto.EmployeeDto;
+
+public interface EmployeeService {
+    EmployeeDto create(EmployeeDto employeeDto);
+}
